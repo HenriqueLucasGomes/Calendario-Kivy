3e2a6d850280a460742dc272af5ce1d4939adb93
henrique <henriquelucas09@hotmail.com>
2019-02-23 16:05:06 -0300

    comentários adicionados

--- calendario.py
+++ calendario.py
@@ -7,22 +7,35 @@ from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.button import Button
-from kivy.lang import Builder
 
-Window.clearcolor=[1,1,1,1]
-conn=sqlite3.connect(r"C:\dev\calendario_db\calendario.db")
+Window.clearcolor=[1,1,1,1]#define o fundo padrão como brnaco
+#conn=sqlite3.connect(r"C:\dev\calendario_db\calendario.db")#Conexão com o BD no windows
+conn=sqlite3.connect(r"/home/henrique/projetos/Calendario/Calendario-Kivy/calendario.db")#Conexão com o BD no linux
 
-vol=None
-d=None
-nv=None
+
+def text(self, x, o):#exibe as informações sobre o dia
+    di = {"01": "janeiro", "02": "fevereiro", "03": "março", "04": "abril", "05": "maio", "06": "junho",
+          "07": "julho", "08": "agosto", "09": "setembro", "10": "outubro", "11": "novembro", "12": "dezembro"}#dicionário que informa os meses por número
+    global mes
+    mes = di.pop(o)#pega o mes em palavra de acordo com o seu número
+    cursor = conn.execute("select " + mes + " from Calendário_2018 where dia=" + str(x.text))#busca no BD as informações
+    rows = cursor.fetchall()
+    dia = int(x.text)#pega o texto do dia (no caso o numero dele)
+    if (dia >= 1 and dia <= 9):
+        dia = str("0" + str(x.text))
+    self.ids.data.text = str(dia) + "/" + str(o) + "/2018"#define qual a data deve aparecer nas informações
+    global r
+    for f in rows:
+        r = f[0]
+    self.ids.assunto.text = str(r)#informação sobre o dia
+
 class Visualizar(StackLayout):
-    def editar(self):
+    def editar(self):#leva para a classe de edição
         global ed
         ed = Editar()
         ed.visul(nx,no)
-    def visual(self,x,o):
-        tela.root_window.remove_widget(t)  # root_window acessa o topo da hierarquia
+    def visual(self,x,o):#visualiza as informações do dia
+        tela.root_window.remove_widget(c)
         tela.root_window.remove_widget(vol)
         tela.root_window.remove_widget(nv)
         global v
@@ -31,124 +44,75 @@ class Visualizar(StackLayout):
         nx=x
         no=o
         v = Visualizar()
-        v.text(x,o)
+        text(v,x,o)
         tela.root_window.add_widget(v)
 
-    def text(self,x,o):
-        di = {"01": "janeiro", "02": "fevereiro", "03": "março", "04": "abril", "05": "maio", "06": "junho",
-              "07": "julho", "08": "agosto", "09": "setembro", "10": "outubro", "11": "novembro", "12": "dezembro"}
-        mes=di.pop(o)
-        cursor = conn.execute("select "+mes+" from Calendário_2018 where dia="+str(x.text))
-        rows = cursor.fetchall()
-        dia=int(x.text)
-        if(dia>=1 and dia<=9):
-            dia = str("0" + str(x.text))
-        self.ids.data.text=str(dia)+"/"+str(o)+"/2018"
-        global r
-        for f in rows:
-            r=f[0]
-        self.ids.assunto.text=str(r)
-    def voltar(self):
-        tela.root_window.remove_widget(v)  # root_window acessa o topo da hierarquia
+    def voltar(self):#volta para o calendario
+        tela.root_window.remove_widget(v)
         global vol
         vol= Calendario()
         vol.ver()
         tela.root_window.add_widget(vol)
 
-nv=None
 
 
-class Editar(StackLayout):
-    def visul(self, x, o):
-        tela.root_window.remove_widget(v)  # root_window acessa o topo da hierarquia
+class Editar(StackLayout):#ativa a edição das informações do dia
+    def visul(self, x, o):#tela de edição
+        tela.root_window.remove_widget(v)
         tela.root_window.remove_widget(nv)
         tela.root_window.remove_widget(d)
         global e
         e = Editar()
-        e.text(x, o)
+        text(e,x, o)
         tela.root_window.add_widget(e)
 
-    def text(self, x, o):
-        di = {"01": "janeiro", "02": "fevereiro", "03": "março", "04": "abril", "05": "maio", "06": "junho",
-              "07": "julho", "08": "agosto", "09": "setembro", "10": "outubro", "11": "novembro", "12": "dezembro"}
-        global mes
-        mes = di.pop(o)
-        cursor = conn.execute("select " + mes + " from Calendário_2018 where dia=" + str(x.text))
-        rows = cursor.fetchall()
-        dia = int(x.text)
-        if (dia >= 1 and dia <= 9):
-            dia = str("0" + str(x.text))
-        self.ids.data.text = str(dia) + "/" + str(o) + "/2018"
-        global r
-        for f in rows:
-            r = f[0]
-        self.ids.assunto.text = str(r)
-
-    def cancelar(self):
-        tela.root_window.remove_widget(e)  # root_window acessa o topo da hierarquia
+    def cancelar(self):#cancela as alterações
+        tela.root_window.remove_widget(e)
         global nv
         nv = Calendario()
         nv.ver()
         tela.root_window.add_widget(nv)
-    def salvar(self):
-        assunto=self.ids.assunto.text
+    def salvar(self):#salva as alterações
+        assunto=self.ids.assunto.text#pega o que foi registrado
         dia=nx.text
         conn.execute(
-        "update Calendário_2018 set " + mes + "='" + assunto + "' where dia=" + str(dia))
-        conn.commit()
-        tela.root_window.remove_widget(e)
+        "update Calendário_2018 set " + mes + "='" + assunto + "' where dia=" + str(dia))#registra as alterações no BD
+        conn.commit()#salva o registro
+        tela.root_window.remove_widget(e)#remove um widget
         global d
         d=Visualizar()
         d.visual(nx,no)
 
 
 class Calendario(StackLayout):
-    def dia(self,x,o):
-       i=Visualizar()
-       i.visual(x,o)
-    def ver(self):
-        tela.root_window.remove_widget(tela.root)  # root_window acessa o topo da hierarquia
-        global t
-        t = Calendario()
-        tela.root_window.add_widget(t)
+    def dia(self,x,o):#ativa ao clicar em qualquer dia
+       v=Visualizar()
+       v.visual(x,o)
+    def ver(self):#volta a tela do calendário
+        tela.root_window.remove_widget(tela.root)  # root_window acessa o topo da hierarquia, remove o topo da hierarquia
+        global c
+        c = Calendario()
+        tela.root_window.add_widget(c)#adiciona um novo widget
 
 
-class Tela(FloatLayout):
-    def botao1(self):
-       i=Calendario()
-       i.ver()
-
-    def botao2(self):
-        try:
-            a = 10 / 0
-        except ZeroDivisionError:
-            pass
+class Tela(FloatLayout):#primeira tela
+    def ver(self):#botão Ver Calendario
+       c=Calendario()
+       c.ver()#Chama a tela do calendario
 
 
-class CalApp(App):
-    pass
+class CalApp(App):#classe princiapl que imorta o arquivo kivy
+    #inicialização das variaveis
+    global vol
+    global d
+    global nv
+    global c
+    c=None
+    vol = None
+    d = None
+    nv = None
 
 tela=CalApp()
 tela.title="Calendário"
 tela.run()
 conn.close()
-
-#print("Bem Vindo ao Calendário Anula de 2018!\nOpções:\n1-Ver meu Calendário\n2-Editar meu Calendário\n3-Sair")
-#  def botao1(self):
-#        cursor = conn.execute("select * from Calendário_2018")
-#        rows = cursor.fetchall()
-#        for row in rows:
-#            print(row)
-#
-# def botao1(self):
-#    return Calendario()
-#def botao2(self):
-#    assunto = input("Qual o assunto do dia?\n")
-#   mes = input("Qual mes refe
-# rente?\n")
-#    dia = input("Qual dia referente?\n")
-#    conn.execute(
-#        "update Calendário_2018 set " + mes + "='" + assunto + "' where dia=" + str(dia))  # maique gambiarra buníta so
-#    conn.commit()
-
-
